6c51d35adc50b5c9012b43a60dbae8e6a4c35295
TyshchenkoYulia <veshkina.yulia021088@gmail.com>
2024-01-28 15:12:16 +0200

    add task 5

---
+++ js/task-5.js
@@ -0,0 +1,26 @@
+'use script';
+
+
+
+const widget = document.querySelector('.widget');
+const spanColor = document.querySelector('.color');
+
+
+function getRandomHexColor() {
+
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+
+}
+  
+widget.addEventListener("click", onButtonClick);
+
+function onButtonClick() {
+    
+    const changeColor = getRandomHexColor();
+
+    document.body.style.backgroundColor = changeColor;
+
+    spanColor.textContent = changeColor;
+}
