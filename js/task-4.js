ce7e34d72f6f7c475ed590cdb99cf52e88dd0ecd
TyshchenkoYulia <veshkina.yulia021088@gmail.com>
2024-01-28 14:29:32 +0200

    finish task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,34 +1,32 @@
 'use strict';
 
-// Обробка відправлення форми form.login-form повинна відбуватися 
-// за подією submit.
-
-// Під час відправлення форми сторінка не повинна перезавантажуватися.
-
-// Якщо при сабміті у формі є незаповнені поля, виводь alert з 
-// попередженням про те, що 'All form fields must be filled in'. 
-// Не додавай на інпути атрибут required, валідація має відбуватися 
-// саме через JS.
-
-// Якщо користувач заповнив усі поля і відправив форму,
-//  збери значення полів в об'єкт з двома властивостями, 
-//  де ключ — це ім'я інпутів, а значення — відповідні значення цих 
-//  інпутів, очищені від пробілів по краях. 
-//  Для доступу до елементів форми використовуй властивість elements.
-
-// При сабміті форми виведи об'єкт із введеними даними в консоль 
-// і очисти значення полів форми методом reset.
-
 const form = document.querySelector('.login-form');
 
-function formHandler(event) {
+function onCheckForm(event) {
     event.preventDefault();
-    console.log(event);
+
+    const email = event.target.email.value.trim();
+    const password = event.target.password.value.trim();
+
+    if(email === "" || password === "") {
+        alert('All form fields must be filled in');
+    }
+
+    const submitForm = {
+        email: `${email}`,
+        password: `${password}`,
+    }
+
+   console.log(submitForm) 
+   form.reset();
 }
 
-form.addEventListener("submit", formHandler);
+form.addEventListener("submit", onCheckForm);
 
 
 
-console.log(form);
+
+
+
+
 
